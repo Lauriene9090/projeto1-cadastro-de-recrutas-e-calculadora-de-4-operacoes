f7156498761eeafc649d3a72b90e54d8741bb846
Lauriene <laurienea840@gmail.com>
2024-09-01 11:30:03 -0300

    Add files via upload

---
+++ cadastro de recrutas/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("digite seu primeiro nome")
+let sobrenome = prompt("digite seu segundo nome")
+let curso = prompt("digite seu curso")
+let anodenascimento = parseInt(prompt("digite seu ano de nascimento"))
+let idade = 2024 - anodenascimento
+
+alert(' olá ' +  nome  + ' ' + sobrenome  +
+      ' então voce estuda ' + curso + ' e tem ' + idade + ' anos. ' + ' legal! ')
